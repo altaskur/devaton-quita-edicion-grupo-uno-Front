1ccd46c3dd57bcefe8b71fe8793cb2181a02abca
rogmar-rivero <rogmarramsesriveromoyetones@gmail.com>
2023-07-31 16:05:11 -0400

    fix: strcuture standalone

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ROUTES } from '@core/routes/routes';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      HttpClientModule,
+      BrowserModule,
+      BrowserAnimationsModule
+    ),
+    provideRouter(ROUTES),
+  ],
+}).catch(err => console.log(err));
