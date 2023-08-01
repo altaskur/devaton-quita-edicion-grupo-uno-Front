96bf893e9715824b7c3e5956e726b0f29d817b38
Miguel Decode <migueldecode@gmail.com>
2023-08-01 22:42:22 +0200

    fix: remove comments from login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  // imports: [CommonModule],
   imports: [],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
