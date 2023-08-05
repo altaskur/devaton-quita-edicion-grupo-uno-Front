cdfadc15224bf53937de12896e508bf42dc3ed66
Miguel Decode <migueldecode@gmail.com>
2023-08-05 18:22:31 +0200

    feat: create layout of navbar

---
+++ src/app/core/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent {
+  ISAUTH: boolean = false;
+
+  toggleMenu() {
+    console.log('Menu clicked');
+  }
+}
