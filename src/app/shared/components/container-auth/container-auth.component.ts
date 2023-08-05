7fe1517b69947429124050fdd79dc7d3be32e55c
rogmar-rivero <rogmarramsesriveromoyetones@gmail.com>
2023-08-04 20:03:26 -0400

    feat: wrapper component

---
+++ src/app/shared/components/container-auth/container-auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+//import { } from '../../../../assets/Header.png'
+
+@Component({
+  selector: 'app-container-auth',
+  standalone: true,
+  templateUrl: './container-auth.component.html',
+  styleUrls: ['./container-auth.component.css'],
+})
+export class ContainerAuthComponent {}
