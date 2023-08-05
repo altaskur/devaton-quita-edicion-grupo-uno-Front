6fd3cfe369cd335a393e492f2ae7e6d82fb55223
Rogmar Rivero <rogmarramsesriveromoyetones@gmail.com>
2023-08-04 20:12:16 -0400

    Update button.component.ts
    fix button type submit
    to generic

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -5,7 +5,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   standalone: true,
   template: `
     <button
-      type="submit"
       (click)="goToAction()"
       class="bg-orange-500 my-5 hover:bg-orange-600 transition duration-300 ease-in-out
        text-white font-bold rounded-full px-4 py-2 focus:outline-none w-full">
