4e9bc4d7e6cc970777e9541a8c6c9129648f7a8f
rogmar-rivero <rogmarramsesriveromoyetones@gmail.com>
2023-08-06 22:37:46 -0400

    fix: styles for forgot password

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ButtonComponent } from '@shared/components/button/button.component';
+import { ContainerAuthComponent } from '@shared/components/container-auth/container-auth.component';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ContainerAuthComponent, ButtonComponent],
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css'],
 })
