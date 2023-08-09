15aa6cba552a40926829a14954917705609106a9
Franklin Gil <imfranq.ve@gmail.com>
2023-08-09 14:28:35 -0400

    feat: create footer interface

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '@shared/components/button/button.component';
+import { FooterComponent } from '@shared/components/footer/footer.component';
 import { RatingsStarComponent } from '@shared/components/ratings-star/ratings-star.component';
 import { SvgImageComponent } from '@shared/components/svg-image/svg-image.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonComponent, SvgImageComponent, RatingsStarComponent],
+  imports: [ButtonComponent, SvgImageComponent, RatingsStarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
