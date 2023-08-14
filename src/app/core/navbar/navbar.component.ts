47d33829049a513e73f6997b60806825348d04f4
Miguel Decode <migueldecode@gmail.com>
2023-08-14 22:10:27 +0200

    fix: change svg inline for app-svg-image component

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { SvgImageComponent } from '@shared/components/svg-image/svg-image.component';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, SvgImageComponent],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
