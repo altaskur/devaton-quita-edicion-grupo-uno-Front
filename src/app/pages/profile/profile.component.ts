2b0fb7cd07d84f54843e2a67a69033a69ed61829
Miguel Decode <migueldecode@gmail.com>
2023-08-17 23:05:01 +0200

    feat: first prototype layout of profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RatingsStarComponent } from '@shared/components/ratings-star/ratings-star.component';
+import { ButtonComponent } from '@shared/components/button/button.component';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RatingsStarComponent, ButtonComponent],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
